a9d1b027f99b6b9f862d2adbd1444637761dabb5
cherry <1411029255@qq.com>
2019-09-27 18:28:25 +0800

    create a ForArray class

---
+++ src/com/apple/ForArray/ForArray.java
@@ -0,0 +1,10 @@
+public class ForArray{
+	
+	public ststic void main(String[] args){
+		int[] array = new int [(int)(Math.random() * 10) + 1];
+		for(int i = 0;i <= array.length; ++i)
+			array[i] = (int)(Math.random() * 100);
+		for(int value;array)
+			System.out.println(value);
+	}
+}
